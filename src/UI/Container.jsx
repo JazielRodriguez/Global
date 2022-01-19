15c5102b61c5d2d878f2202d78e81f2c60ae1a56
JazielRodriguez <78989856+JazielRodriguez@users.noreply.github.com>
2022-01-19 15:00:54 -0600

    Add files via upload

---
+++ src/UI/Container.jsx
@@ -0,0 +1,5 @@
+import styles from './Container.module.css'
+const Container = (props) => {
+  return <div>{props.children}</div>
+}
+export default Container
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Container from "../UI/Container";
+import styles from "./Header.module.css";
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Container>
+        <div>
+          <h1>React Boilerplate</h1>
+        </div>
+      </Container>
+    </header>
+  );
+};
+export default Header
